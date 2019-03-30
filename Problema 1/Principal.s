93b50f7d61248a324e95e296e44c202b01bec2f9
Antonio Raian <raymendesjr2013@gmail.com>
2019-03-30 19:28:23 -0300

    Primeiros passos

---
+++ Problema 1/Principal.s
@@ -0,0 +1,20 @@
+.data
+
+.global main
+
+.text
+
+main:
+	#botão desce
+	movi r4, 50 	#r4 tem a posição 50 da memória
+	ldw r3, 0[r4]	#r3 tem o valor q está na memória de r4
+	bne r3, r0, desce
+	#botão sobe
+	ldw r5, 4[r4]	#r3 tem o valor q está na memória de r4
+	bne r5, r0, sobe
+	br main
+desce:
+
+sobe:
+
+.end
